0b13313b83c31599a66ab82fb874a2b5818a9685
yu sato <yu_sato@yumemi.co.jp>
2020-03-12 13:17:23 +0900

    add APNGKit

--- APNGKitSample/ViewController.swift
+++ APNGKitSample/ViewController.swift
@@ -1,13 +1,24 @@
 
 import UIKit
 
+import APNGKit
+
 class ViewController: UIViewController {
+
+    @IBOutlet private weak var imageView: APNGImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupImageView()
     }
 
+    private func setupImageView() {
+        guard let path = Bundle.main.path(forResource: "elephant", ofType: "apng") else { return }
+        
+        let image = APNGImage(contentsOfFile: path)
+        
+        imageView.image = image
+        imageView.startAnimating()
+    }
 
 }
-
